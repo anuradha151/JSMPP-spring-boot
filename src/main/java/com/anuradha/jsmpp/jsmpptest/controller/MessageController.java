31badadc180bbe6fd0c2e8987e6c0bef31006836
anuradha151 <anuranasinghe151@gmail.com>
2018-10-12 14:05:46 +0530

    message controller and service annotate

--- src/main/java/com/anuradha/jsmpp/jsmpptest/controller/MessageController.java
+++ src/main/java/com/anuradha/jsmpp/jsmpptest/controller/MessageController.java
@@ -1,4 +1,9 @@
 package com.anuradha.jsmpp.jsmpptest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
 public class MessageController {
 }
--- src/main/java/com/anuradha/jsmpp/jsmpptest/service/impl/MessageServiceImpl.java
+++ src/main/java/com/anuradha/jsmpp/jsmpptest/service/impl/MessageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.anuradha.jsmpp.jsmpptest.service.impl;
 
 import com.anuradha.jsmpp.jsmpptest.service.MessageService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MessageServiceImpl implements MessageService {
 }
