9c828fd4763db8ee7b70e43ba8c7a0bea12a90bb
anuradha151 <anuranasinghe151@gmail.com>
2018-10-12 13:58:06 +0530

    first commit

---
+++ src/main/java/com/anuradha/jsmpp/jsmpptest/JsmppTestApplication.java
@@ -0,0 +1,12 @@
+package com.anuradha.jsmpp.jsmpptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsmppTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsmppTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/anuradha/jsmpp/jsmpptest/JsmppTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anuradha.jsmpp.jsmpptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsmppTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
